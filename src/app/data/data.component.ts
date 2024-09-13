b8bdb99c5a7349bb823f50ea1bb62b99492d5728
Fekete János Dávid <janosdavidfekete@gmail.com>
2024-09-13 11:32:33 +0200

    Még mindig műkdöik :)

--- src/app/data/data.component.ts
+++ src/app/data/data.component.ts
@@ -8,13 +8,34 @@ import { ConfigService } from '../config.service';
   styleUrl: './data.component.css'
 })
 export class DataComponent {
-  todos:any
-  oszlopok:any
+ todos:any
+ newTodo:any={}
+ oszlopok:any
 
-  constructor(private base:BaseService, private config:ConfigService){
-    this.base.getTodo().subscribe(
-      (adatok)=>this.todos=adatok
-    )
-    this.oszlopok=config.getTodoColumns()
+constructor(private base:BaseService, private config:ConfigService){
+  this.base.getTodo().subscribe(
+    (adatok)=>this.todos=adatok
+  )
+  this.oszlopok=config.getTodoColumns()
+}
+
+ment(todo:any){
+  todo.userId=Number(todo.userId)
+  console.log(todo)
+}
+
+torol(todo:any){
+  console.log(todo)
+}
+hozzaad(){
+  if (Object.keys(this.newTodo).length !== 0){
+    this.newTodo.userId=Number(this.newTodo.userId)
+    console.log(this.newTodo)
+    this.newTodo={}
   }
+  else{
+    console.log("Hülye a user!")
+  }
+  // HU
+}
 }
